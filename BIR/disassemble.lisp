6d73832ecd22615073a35a6bd65e8b8b50ca8986
Bike <aeshtaer@gmail.com>
2020-09-03 16:08:56 -0400

    Disassemble nested functions

--- BIR/disassemble.lisp
+++ BIR/disassemble.lisp
@@ -1,5 +1,16 @@
 (in-package #:cleavir-bir)
 
+(defvar *seen*)
+(defvar *work*)
+
+(defun maybe-add-disassemble-work (function)
+  (check-type function function)
+  (unless (presentp function *seen*)
+    (push function *work*)))
+
+(defvar *disassemble-nextv*)
+(defvar *disassemble-vars*)
+
 (defvar *disassemble-nextn*)
 (defvar *disassemble-ids*)
 
@@ -9,12 +20,18 @@
             (prog1 *disassemble-nextn*
               (incf *disassemble-nextn*)))))
 
+(defun dis-var (variable)
+  (or (gethash variable *disassemble-vars*)
+      (setf (gethash variable *disassemble-vars*)
+            (prog1 (make-symbol (write-to-string *disassemble-nextv*))
+              (incf *disassemble-nextv*)))))
+
 (defgeneric dis-label (instruction))
 (defmethod dis-label ((instruction instruction))
   (class-name (class-of instruction)))
 
 (defgeneric disassemble-variable (variable))
-(defmethod disassemble-variable ((v variable)) (list (dis-id v)))
+(defmethod disassemble-variable ((v variable)) (dis-var v))
 
 (defgeneric disassemble-value (value))
 
@@ -41,12 +58,20 @@
                (disassemble-variable (variable inst))
                (mapcar #'disassemble-value (inputs inst)))))
 
+(defmethod disassemble-instruction ((inst enclose))
+  (maybe-add-disassemble-work (code inst))
+  `(:= ,(disassemble-value inst)
+       ,(list* (dis-label inst)
+               (code inst)
+               (mapcar #'disassemble-value (inputs inst)))))
+
 (defun disassemble-iblock (iblock)
   (check-type iblock iblock)
   (let ((insts nil))
     (mapnil-instructions (lambda (i) (push (disassemble-instruction i) insts))
                          (start iblock))
-    (list* iblock (nreverse insts))))
+    (list* (list* iblock (mapcar #'disassemble-value (inputs iblock)))
+           (nreverse insts))))
 
 (defun disassemble-function (function)
   (check-type function function)
@@ -55,4 +80,15 @@
         (*disassemble-nextn* 0))
     (mapset (lambda (b) (push (disassemble-iblock b) iblocks))
             (iblocks function))
-    (list* function iblocks)))
+    (list* (list* function (mapcar #'disassemble-value (inputs function)))
+           iblocks)))
+
+(defun disassemble-ir (ir)
+  (check-type ir function)
+  (let ((*seen* (make-set ir))
+        (*work* (list ir))
+        (*disassemble-nextv* 0)
+        (*disassemble-vars* (make-hash-table :test #'eq)))
+    (loop for work = (pop *work*)
+          until (null work)
+          collect (disassemble-function work))))
