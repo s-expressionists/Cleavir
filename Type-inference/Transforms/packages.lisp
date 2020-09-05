f0c5bb2920b837083127d590711f87e3e190316b
Bike <aeshtaer@gmail.com>
2020-09-05 15:06:27 -0400

    Delete some old, redundant systems

--- Type-inference/Transforms/packages.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:cleavir-typed-transforms
-  (:use #:common-lisp #:cleavir-type-inference)
-  (:export #:insert-type-checks)
-  (:export #:thes->typeqs #:delete-the)
-  (:export #:prune-typeqs #:prune-the)
-  (:export #:mvcs->funcalls))
