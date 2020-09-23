ea9843e309f127f2bd07c51788ac3a49f8daa774
Bike <aeshtaer@gmail.com>
2020-09-23 11:52:30 -0400

    Delete old AST-to-BIR

--- AST-to-BIR/packages.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:cleavir-ast-to-bir
-  (:use #:cl)
-  (:shadow #:function)
-  (:export #:compile-toplevel #:compile-function
-           #:compile-ast #:compile-test-ast)
-  (:export #:return-1))
