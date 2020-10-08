09c74c75ea9512ded83904ce33a1c25fb9eaf8ca
Charles Zhang <charleszhang99@yahoo.com>
2020-10-08 16:58:22 -0700

    Add a stupid local call analysis.

--- BIR-transformations/process-captured-variables.lisp
+++ BIR-transformations/process-captured-variables.lisp
@@ -50,3 +50,41 @@
 (defun process-captured-variables (ir)
   (let ((af (cleavir-bir:functions (cleavir-bir:module ir))))
     (transmit-variables af)))
+
+
+;; Find all calls to a function and mark them as local calls.
+
+;; FIXME: Right now, this runs on functions which are explicitly not
+;; closures, so this needs to happen after p-c-v. This is rather
+;; suboptimal, since the true power of local calls lies in eliding
+;; heap allocated closures. Otherwise we can run this straight away
+;; and have p-c-v do lambda lifting on local calls.
+(defun find-local-calls (function)
+  (cleavir-set:doset (enclose (cleavir-bir:encloses function))
+    (let ((use (cleavir-bir:use enclose))
+          (external-reference-p nil))
+      (when (cleavir-set:empty-set-p (cleavir-bir:variables enclose))
+        (typecase use
+          (cleavir-bir:call
+           (change-class use 'cleavir-bir::local-call)
+           (cleavir-bir:replace-computation enclose function))
+          (cleavir-bir:writevar
+           (let ((variable (first (cleavir-bir:outputs use))))
+             (when (cleavir-bir:immutablep variable)
+               (cleavir-set:doset (reader (cleavir-bir:readers variable))
+                 (let ((use (cleavir-bir:use reader)))
+                   (typecase use
+                     (cleavir-bir:call
+                      (when (or (eq use (cleavir-bir:callee use))
+                                (not (member reader (rest (cleavir-bir:inputs use)))))
+                        (change-class use 'cleavir-bir::local-call)
+                        (cleavir-bir:replace-computation reader function)))
+                     (t
+                      (setq external-reference-p t)))))))
+           (unless external-reference-p
+             (cleavir-bir:delete-instruction use))))
+        (unless external-reference-p
+          (cleavir-bir:delete-computation enclose))))))
+
+(defun local-call-analyze (ir)
+  (cleavir-set:mapset nil #'find-local-calls (cleavir-bir:functions (cleavir-bir:module ir))))
