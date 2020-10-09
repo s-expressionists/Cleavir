a205960d0f99981d8617aa62ec4ef7ada15a8935
Charles Zhang <charleszhang99@yahoo.com>
2020-10-09 13:03:15 -0700

    pcv: Avoid consing new sets a bunch.
    The membership test makes doing so moot.

--- BIR-transformations/process-captured-variables.lisp
+++ BIR-transformations/process-captured-variables.lisp
@@ -1,12 +1,5 @@
 (in-package #:cleavir-bir-transformations)
 
-(defun owners (&rest use-sets)
-  (let ((owners (cleavir-set:empty-set)))
-    (dolist (use-set use-sets)
-      (cleavir-set:doset (use use-set)
-        (cleavir-set:nadjoinf owners (cleavir-bir:function use))))
-    owners))
-
 ;;; Add VARIABLE onto the environment of ACCESS-FUNCTION and do so
 ;;; recursively.
 (defun close-over-variable (function access-function variable)
@@ -14,17 +7,19 @@
     (unless (or (eq function access-function)
                 (cleavir-set:presentp variable environment))
       (cleavir-set:nadjoinf environment variable)
-      (cleavir-set:doset (use (owners (cleavir-bir:encloses access-function)
-                                      (cleavir-bir:local-calls access-function)))
-        (close-over-variable function use variable)))))
+      (cleavir-set:doset (enclose (cleavir-bir:encloses access-function))
+        (close-over-variable function (cleavir-bir:function enclose) variable))
+      (cleavir-set:doset (local-call (cleavir-bir:local-calls access-function))
+        (close-over-variable function (cleavir-bir:function local-call) variable)))))
 
 ;;; Fill in the environments of every function.
 (defun process-captured-variables (ir)
   (cleavir-set:doset (function (cleavir-bir:functions (cleavir-bir:module ir)) (values))
     (cleavir-set:doset (variable (cleavir-bir:variables function))
-      (cleavir-set:doset (access-function (owners (cleavir-bir:readers variable)
-                                                  (cleavir-bir:writers variable)))
-        (close-over-variable function access-function variable)))))
+      (cleavir-set:doset (reader (cleavir-bir:readers variable))
+        (close-over-variable function (cleavir-bir:function reader) variable))
+      (cleavir-set:doset (writer (cleavir-bir:writers variable))
+        (close-over-variable function (cleavir-bir:function writer) variable)))))
 
 ;; Find all calls to a function and mark them as local calls.
 
