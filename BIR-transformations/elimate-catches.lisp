b3deff53e96adf53dcd0673efb8351b9101708fb
Bike <aeshtaer@gmail.com>
2020-10-13 17:29:51 -0400

    Delete stray iblocks when deleting catch

--- BIR-transformations/elimate-catches.lisp
+++ BIR-transformations/elimate-catches.lisp
@@ -4,15 +4,19 @@
   (cleavir-set:empty-set-p (cleavir-bir:unwinds catch)))
 
 (defun eliminate-catch (catch)
-  (let ((nde (cleavir-bir:dynamic-environment catch)))
+  (let ((nde (cleavir-bir:dynamic-environment catch))
+        (normal-next (first (cleavir-bir:next catch)))
+        (other-next (rest (cleavir-bir:next catch))))
     (cleavir-set:doset (s (cleavir-bir:scope catch))
       (setf (cleavir-bir:dynamic-environment s) nde))
     ;; Replace the instruction
     (cleavir-bir:replace-terminator
      (make-instance 'cleavir-bir:jump
        :inputs () :outputs () :unwindp nil
-       :next (list (first (cleavir-bir:next catch))))
-     catch)))
+       :next (list normal-next))
+     catch)
+    ;; Other blocks might be unreachable now
+    (mapc #'cleavir-bir:maybe-delete-iblock other-next)))
 
 (defun eliminate-catches (function)
   (cleavir-bir:map-iblocks
