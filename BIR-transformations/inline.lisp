4d043b0e5e82ce8309d4ebef51ee8a249f51d2a8
Bike <aeshtaer@gmail.com>
2020-10-13 15:05:22 -0400

    Don't lose on unused functions

--- BIR-transformations/inline.lisp
+++ BIR-transformations/inline.lisp
@@ -5,18 +5,19 @@
 ;; interpolatability specifically (so e.g. there must be only one call).
 (defun potential-inlines (enclose)
   (check-type enclose cleavir-bir:enclose)
-  (let* ((use (cleavir-bir:use enclose))
-         (use-inputs (cleavir-bir:inputs use)))
-    (typecase use
-      (cleavir-bir:call
-       ;; If it's only used for a call, it must be inlinable, as it must be in
-       ;; the same function as the enclose i.e. not recursive.
-       ;; It does have to be the callee and only the callee, though.
-       ;; Since enclose is linear, it only has one use, so we only need to
-       ;; check that it's the callee.
-       (when (eq enclose (first use-inputs))
-         (list (cleavir-bir:use enclose))))
-      (t nil))))
+  (unless (cleavir-bir:unused-p enclose)
+    (let* ((use (cleavir-bir:use enclose))
+           (use-inputs (cleavir-bir:inputs use)))
+      (typecase use
+        (cleavir-bir:call
+         ;; If it's only used for a call, it must be inlinable, as it must be in
+         ;; the same function as the enclose i.e. not recursive.
+         ;; It does have to be the callee and only the callee, though.
+         ;; Since enclose is linear, it only has one use, so we only need to
+         ;; check that it's the callee.
+         (when (eq enclose (first use-inputs))
+           (list (cleavir-bir:use enclose))))
+        (t nil)))))
 
 ;; required parameters only. rip.
 (defun lambda-list-inlinable-p (lambda-list)
