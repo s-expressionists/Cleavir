68682ded2a1c8ea5f27e4fe4f67a7782af95063a
Bike <aeshtaer@gmail.com>
2020-11-02 10:22:13 -0500

    Add optional list-based set implementation

--- Set/set.lisp
+++ Set/set.lisp
@@ -1,50 +1,85 @@
 (in-package #:cleavir-set)
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (push #+(or):listset #-(or):hashset *features*))
+
 ;;; hash sets, with a simple wrapper so they print nicely
 (defclass set ()
-  ((%hash :initarg :hash :reader %hash)))
+  (#+listset(%list :initarg :list :accessor %setlist)
+   #+hashset(%hash :initarg :hash :reader %hash)))
 
 ;; internal to this file
-;; FIXME: should be weak key?
+#+hashset
 (declaim (inline hashset))
+#+hashset
 (defun hashset () (make-hash-table :test #'eq))
 
-(defun empty-set () (make-instance 'set :hash (hashset)))
+(defun empty-set ()
+  #+hashset
+  (make-instance 'set :hash (hashset))
+  #+listset
+  (make-instance 'set :list nil))
 
 (defun empty-set-p (set)
-  (zerop (hash-table-count (%hash set))))
+  #+hashset
+  (zerop (hash-table-count (%hash set)))
+  #+listset
+  (null (%setlist set)))
 
 (defun size (set)
-  (hash-table-count (%hash set)))
+  #+hashset
+  (hash-table-count (%hash set))
+  #+listset
+  (length (%setlist set)))
 
 (defun make-set (&rest elements)
+  #+hashset
   (loop with r = (hashset)
         for e in elements
         do (setf (gethash e r) t)
-        finally (return (make-instance 'set :hash r))))
+        finally (return (make-instance 'set :hash r)))
+  #+listset
+  (make-instance 'set :list (delete-duplicates elements)))
 
 (defun presentp (item set)
-  (values (gethash item (%hash set))))
+  #+hashset
+  (values (gethash item (%hash set)))
+  #+listset
+  (member item (%setlist set)))
 
 (defmacro doset ((var setform &optional result) &body body)
+  #+hashset
   `(loop for ,var being the hash-keys of (%hash ,setform)
          do (tagbody ,@body)
-         finally (return ,result)))
+         finally (return ,result))
+  #+listset
+  `(dolist (,var (%setlist ,setform) ,result) ,@body))
 
 (defun mapset (result-type f set)
   (ecase result-type
     ((nil) (doset (item set nil) (funcall f item)))
-    ((set) (let ((new (hashset)))
-             (doset (item set (make-instance 'set :hash new))
-               (setf (gethash (funcall f item) new) t))))
-    ((list) (let (r) (doset (item set r) (push (funcall f item) r))))))
+    ((set)
+     #+hashset
+     (let ((new (hashset)))
+       (doset (item set (make-instance 'set :hash new))
+              (setf (gethash (funcall f item) new) t)))
+     #+listset
+     (make-instance 'set :list (mapcar f (%setlist set))))
+    ((list)
+     #+hashset
+     (let (r) (doset (item set r) (push (funcall f item) r)))
+     #+listset
+     (mapcar f (%setlist set)))))
 
 (defun set-to-list (set) (mapset 'list #'identity set))
 
 (defun copy-set (set)
+  #+hashset
   (let ((result (hashset)))
     (doset (e set (make-instance 'set :hash result))
-      (setf (gethash e result) t))))
+      (setf (gethash e result) t)))
+  #+listset
+  (make-instance 'set :list (copy-list (%setlist set))))
 
 ;;; Get an arbitrary element of the set, or else no values.
 (defun arb (set)
@@ -64,7 +99,10 @@
     (unless (funcall p i) (return nil))))
 
 (defun nadjoin (item set)
+  #+hashset
   (setf (gethash item (%hash set)) t)
+  #+listset
+  (pushnew item (%setlist set))
   set)
 
 ;;; define-modify-macro reorders arguments and i don't like it
@@ -78,7 +116,10 @@
 ;; Removes an item from the set, returns the new set. Can be (is) destructive.
 ;; could be set-delete, but n is regular, if arbitrary
 (defun nremove (item set)
+  #+hashset
   (remhash item (%hash set))
+  #+listset
+  (setf (%setlist set) (delete item (%setlist set) :count 1))
   set)
 
 (defmacro nremovef (set item &environment env)
@@ -90,7 +131,10 @@
 
 ;; Return an empty set, possibly destroying an existing set to do it.
 (defun nset-empty (set)
+  #+hashset
   (clrhash (%hash set))
+  #+listset
+  (setf (%setlist set) nil)
   set)
 
 ;; Only the first argument is destroyed.
@@ -135,3 +179,7 @@
 (defmethod print-object ((s set) stream)
   (print-unreadable-object (s stream :type t)
     (format stream "{~{~a~^ ~}}" (set-to-list s))))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (setf *features*
+        (remove #+listset :listset #+hashset :hashset *features*)))
