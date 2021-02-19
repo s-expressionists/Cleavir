cd4f68ba6a92253258b1b49ad3f4765607af67ab
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2021-02-19 17:18:51 +0100

    Hack around inability to process (function * ...) types in cleavir-cst-to-ast

--- CST-to-AST/convert-cst.lisp
+++ CST-to-AST/convert-cst.lisp
@@ -97,7 +97,10 @@
          (origin (cst:source cst))
          ;; FIXME: This should be the intersection of all function
          ;; type entries in the environment.
-         (ftype (first (cleavir-env:function-type env info))))
+         (ftype (let ((type (first (cleavir-env:function-type env info))))
+                  (if (eq (second type) '*)
+                      `(function (&rest t) ,(third type))
+                      type))))
     (let ((required (cleavir-ctype:function-required ftype system))
           (optional (cleavir-ctype:function-optional ftype system))
           (rest (cleavir-ctype:function-rest ftype system))
