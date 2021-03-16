f9dd22547f15eef5f323df5c25662b7d224b4bb3
Bike <aeshtaer@gmail.com>
2021-03-16 09:12:06 -0400

    Show instruction disassemblies in BIR verification failure

--- BIR/condition-reporters-english.lisp
+++ BIR/condition-reporters-english.lisp
@@ -17,30 +17,59 @@
           (asserted-type condition)
           (asserted-by condition)))
 
+(defun group-problems (problems)
+  ;; Group by subject. Also, put function problems before iblock problems, and
+  ;; both before instruction problems.
+  (let ((table (make-hash-table :test #'eq)))
+    (dolist (problem problems)
+      (push problem (gethash (subject problem) table)))
+    (let ((list (loop for k being the hash-keys of table
+                        using (hash-value v)
+                      collect (list* k v))))
+      (sort list (lambda (s1 s2)
+                   (etypecase s1
+                     (function t)
+                     (iblock (typep s2 'instruction))
+                     (instruction nil)))
+            :key #'car))))
+
 (defmethod acclimation:report-condition
     ((condition verification-failed) stream (language acclimation:english))
-  (handler-case (let ((*standard-output* stream))
-                  (cleavir-bir-disassembler:display (module condition)))
-    (error (e)
-      (format stream "~&! Error while disassembling module:~@<  ~@;~a~:>" e)))
-  ;; This format string has some complicated bits, so here's a word of
-  ;; explanation. MODULE-PROBLEMS is a list of "problems". FUNCTION-PROBLEMS
-  ;; is a list of (function problem*). A "problem" is a list of a format
-  ;; control and a list of format arguments, suitable for use with ~?.
-  ;; What ~<~{  ~@;~:@{~a~%~}~:> does is: Pop one format argument and use it as
-  ;; the list in a pprint logical block with per-line prefix "  ". This
-  ;; argument is a list of problems.
-  ;; Iterate over each problem passing it to ~?, and then print newline.
-  ;; So given for example problems = (("~hello ~2,'0d" (9)) ("g" ()))
-  ;; you get as output:
-  ;;   hello 09
-  ;;   g
-  (format stream "~&BIR verification failed. The IR is in an inconsistent state.
+  (macrolet ((dis (thing what)
+               `(handler-case (let ((*standard-output* stream))
+                                (cleavir-bir-disassembler:display ,thing))
+                  (error (e)
+                    (format stream
+                            ,(concatenate 'string
+                                          "~&! Error while disassembling "
+                                          what
+                                          ":~@<  ~@;~a~:>")
+                            e)))))
+    (cleavir-bir-disassembler:with-disassembly ()
+      (dis (module condition) "module")
+      (format stream "~&BIR verification failed. The IR is in an inconsistent state.
 This probably indicates a problem in the compiler; please report it.
-~@[~%Problems pertaining to the entire module:~%~<  ~@;~@{~a~%~}~:>~]
-~:{~&Problems pertaining to function ~a:~%~@<  ~@;~@{~a~%~}~:>~}"
-          (module-problems condition)
-          (function-problems condition)))
+~@[~%Problems pertaining to the entire module:~%~<  ~@;~@{~a~%~}~:>~]"
+              (module-problems condition))
+      (when (function-problems condition)
+        (loop for (function . problems) in (function-problems condition)
+              for grouped = (group-problems problems)
+              do (format stream "~&Problems pertaining to function ~a:~%"
+                         function)
+                 (pprint-logical-block (stream grouped :per-line-prefix "  ")
+                   (loop
+                     (pprint-exit-if-list-exhausted)
+                     (destructuring-bind (subject . problems)
+                         (pprint-pop)
+                       (if (typep subject 'instruction)
+                           (dis subject "instruction")
+                           (format stream "~&    ~a" subject))
+                       (fresh-line stream)
+                       (dolist (problem problems)
+                         (apply #'format stream
+                                (problem-format-control problem)
+                                (problem-format-arguments problem))
+                         (terpri stream))))))))))
 
 (defmethod acclimation:report-condition
     ((condition verification-error) stream (language acclimation:english))
