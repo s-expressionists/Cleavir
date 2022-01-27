742d1c26506707ab24b896d1d8dfe84497d17561
Bike <aeshtaer@gmail.com>
2022-01-27 12:57:50 -0500

    CST-to-AST: Fix default special binding code

--- CST-to-AST/convert-special-binding.lisp
+++ CST-to-AST/convert-special-binding.lisp
@@ -9,10 +9,13 @@
 ;;;
 (defmethod convert-special-binding
     (variable-cst value-ast next-ast env system)
-  (convert (cst:quasiquote
-            (cst:source variable-cst)
-            (cleavir-primop:call-with-variable-bound
-             '(cst:unquote variable-cst)
-             (cleavir-primop:ast (cst:unquote value-ast))
-             (lambda () (cleavir-primop:ast (cst:unquote next-ast)))))
-           env system))
+  (let ((source (cst:source variable-cst)))
+    (convert (cst:quasiquote
+              source
+              (cleavir-primop:call-with-variable-bound
+               '(cst:unquote variable-cst)
+               (cleavir-primop:ast
+                (cst:unquote (make-atom-cst value-ast source)))
+               (lambda () (cleavir-primop:ast
+                           (cst:unquote (make-atom-cst next-ast source))))))
+             env system)))
