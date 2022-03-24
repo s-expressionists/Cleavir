8f4a7ac5ee8afc2495303a6e30b392bce208728a
Bike <aeshtaer@gmail.com>
2022-03-24 14:29:22 -0400

    Abstract interpreter: Export symbols

--- Abstract-interpreter/packages.lisp
+++ Abstract-interpreter/packages.lisp
@@ -7,4 +7,23 @@
                     (#:ctype #:cleavir-ctype)
                     (#:attributes #:cleavir-attributes))
   (:shadow #:type)
-  (:export))
+  (:export #:domain
+           #:infimum #:supremum #:subinfop #:join/2 #:meet/2 #:wjoin/2
+           #:meet #:join #:wjoin)
+  (:export #:data #:forward-data #:backward-data
+           #:values-domain #:forward-values-data #:backward-values-data
+           #:control #:forward-control)
+  (:export #:sv-infimum #:sv-supremum #:sv-subinfop
+           #:sv-join/2 #:sv-meet/2 #:sv-wjoin/2
+           #:values-info #:values-required #:values-optional #:values-rest
+           #:info-values-nth #:primary #:single-value)
+  (:export #:flow-datum #:flow-datum-through)
+  (:export #:product #:product-domain #:product-domain-of-type)
+  (:export #:strategy #:optimism #:pessimism
+           #:mark #:flow-call #:interpret-module #:interpret-instruction
+           #:info)
+  (:export #:attribute
+           #:type #:derived-type #:asserted-type
+           #:derive-return-type)
+  (:export #:sequential)
+  (:export #:slots #:sequential-slots))
