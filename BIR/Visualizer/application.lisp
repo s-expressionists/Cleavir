09de4182a63020a34e7d7899e3797d329aef85f6
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2022-03-28 20:37:59 +0200

    In BIR visualizer, allow specifying a sub-sequence of transforms to run

--- BIR/Visualizer/application.lisp
+++ BIR/Visualizer/application.lisp
@@ -80,6 +80,19 @@
                ~0@T        (+ x y)~@
                ~0@T        (list x))))"))
 
+(defvar *all-transforms*
+  '(cleavir-bir-transformations:module-eliminate-catches
+    cleavir-bir-transformations:find-module-local-calls
+    cleavir-bir-transformations:module-optimize-variables
+    (cleavir-bir-transformations:meta-evaluate-module)
+    (cleavir-bir-transformations:module-generate-type-checks)
+    ;; These should happen last since they are like "post passes" which
+    ;; do not modify the flow graph.
+    ;; NOTE: These must come in this order to maximize analysis.
+    cleavir-bir-transformations:determine-function-environments
+    cleavir-bir-transformations:determine-closure-extents
+    cleavir-bir-transformations:determine-variable-extents))
+
 (clim:define-application-frame ir-inspector ()
   ((%cst         :accessor cst)
    (%policy      :accessor policy
@@ -88,6 +101,11 @@
                                  (list 'debug             1)
                                  (list 'space             1)
                                  (list 'compilation-speed 1)))
+   (%all-transforms :initarg  :all-transforms
+                    :reader   all-transforms
+                    :initform *all-transforms*)
+   (%last-transform :accessor last-transform
+                    :initform nil)
    (%output      :accessor output
                  :initform '())
    (%disassembly :accessor disassembly
@@ -121,6 +139,10 @@
                        clim:*application-frame* "Fast"
                        '((speed 3) (safety 0) (debug 0) (space 0) (compilation-speed 0))
                        :background clim:+salmon+))
+   (transforms        :list-pane :items (all-transforms clim:*application-frame*)
+                                 :value-changed-callback
+                                 (lambda (gadget value)
+                                   (setf (last-transform (clim:gadget-client gadget)) value)))
    (ir                 clouseau:inspector-pane)
    (output            output-pane :display-function   'display-output
                                   :end-of-page-action :allow)
@@ -133,6 +155,8 @@
         (clim:horizontally (:spacing 8)
           (:fill (clim:labelling (:label "Lambda Expression")
                    form-editor))
+          (clim:labelling (:label "Transforms")
+            transforms)
           (clim:vertically (:spacing 8)
             speed
             safety
@@ -155,7 +179,7 @@
                    :inherit-from (clouseau:inspector-command-table)))
   (:pretty-name "BIR Inspector"))
 
-(defun update (frame cst policy)
+(defun update (frame cst policy transforms)
   (a:when-let ((ir (clim:find-pane-named frame 'ir)))
     (handler-bind
         ((error (lambda (condition)
@@ -165,11 +189,11 @@
         (setf (values (clouseau:root-object ir :run-hook-p t)
                       (output frame)
                       (disassembly frame))
-              (module<-cst cst policy))))
+              (module<-cst cst policy transforms))))
   nil)
 
 (defmethod (setf cst) :after ((new-value t) (frame ir-inspector))
-  (update frame new-value (policy frame)))
+  (update frame new-value (policy frame) (effective-transforms frame)))
 
 (defmethod (setf policy) :after ((new-value t) (frame ir-inspector))
   (labels ((update-slider (quality)
@@ -178,7 +202,19 @@
                (setf (clim:gadget-value slider :value-changed-callback nil)
                      (second (find quality new-value :key #'first))))))
     (map nil #'update-slider '(speed safety debug space compilation-speed)))
-  (update frame (cst frame) new-value))
+  (update frame (cst frame) new-value (effective-transforms frame)))
+
+(defmethod effective-transforms ((frame ir-inspector))
+  (let* ((all-transforms (all-transforms frame))
+         (last           (last-transform frame))
+         (index          (when last
+                           (position last all-transforms))))
+    (if index
+        (subseq all-transforms 0 (1+ index))
+        all-transforms)))
+
+(defmethod (setf last-transform) :after ((new-value t) (frame ir-inspector))
+  (update frame (cst frame) (policy frame) (effective-transforms frame)))
 
 (defmethod (setf output) :after ((new-value t) (frame ir-inspector))
   (a:when-let ((output (clim:find-pane-named frame 'output)))
--- BIR/Visualizer/compile.lisp
+++ BIR/Visualizer/compile.lisp
@@ -38,32 +38,21 @@
 
 ;;;
 
-(defun bir-transformations (module system)
-  (let ((phases (list ;; 'cleavir-bir-transformations:module-eliminate-catches
-                 ;; 'cleavir-bir-transformations:find-module-local-calls
-                 'cleavir-bir-transformations:module-optimize-variables
-                 (a:rcurry #'cleavir-bir-transformations:meta-evaluate-module system)
-                 ;; cc-bir-to-bmir:reduce-module-typeqs
-                 ;; cc-bir-to-bmir:reduce-module-primops
-                 (a:rcurry #'cleavir-bir-transformations:module-generate-type-checks system)
-                 ;; These should happen last since they are like "post passes" which
-                 ;; do not modify the flow graph.
-                 ;; NOTE: These must come in this order to maximize analysis.
-                 ;; 'cleavir-bir-transformations:determine-function-environments
-                 'cleavir-bir-transformations:determine-closure-extents
-                 'cleavir-bir-transformations:determine-variable-extents)))
-    (reduce (lambda (module transform)
-              (funcall transform module)
-              module)
-            phases :initial-value module)))
+(defun bir-transformations (system module transforms)
+  (reduce (lambda (module transform)
+            (if (consp transform)       ; KLUDGE
+                (funcall (first transform) module system)
+                (funcall transform module)) ; not all transforms return the module
+            module)
+          transforms :initial-value module))
 
 ;;; Form reading and hook into compiler
 
 (defun cst<-string (string)
   (eclector.concrete-syntax-tree:read-from-string string))
 
-(defun module<-cst (cst policy)
-  (let* ((system nil)
+(defun module<-cst (cst policy transforms)
+  (let* ((system :visualizer)
          (output (make-string-output-stream))
          (bir    (let ((*standard-output* output)
                        (*error-output*    output)
@@ -73,12 +62,12 @@
                                         cst environment system)))
                      (cleavir-ast-to-bir:compile-toplevel ast system))))
          (module (bir:module bir))
-         (module (bir-transformations module system)))
+         (module (bir-transformations system module transforms)))
     (values module
             (let ((string (get-output-stream-string output)))
               (if (a:emptyp string) nil string))
             (with-output-to-string (*standard-output*)
               (cleavir-bir-disassembler:display module)))))
 
-(defun module<-string (string policy)
-  (module<-cst (cst<-string string) policy))
+(defun module<-string (string policy transforms)
+  (module<-cst (cst<-string string) policy transforms))
