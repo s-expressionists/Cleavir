fb74eed43ed7aca56bbdfaac3abaf51727a3f0af
Bike <aeshtaer@gmail.com>
2022-06-13 16:58:35 -0400

    Ctype: Export cons readers

--- Ctype/packages.lisp
+++ Ctype/packages.lisp
@@ -21,7 +21,8 @@
            #:values-append/2 #:values-append)
   (:export #:wdisjoin/2 #:wdisjoin #:values-wdisjoin/2 #:values-wdisjoin)
   (:export #:class #:cons #:array #:string
-           #:consp #:arrayp #:array-element-type #:array-dimensions
+           #:consp #:cons-car #:cons-cdr
+           #:arrayp #:array-element-type #:array-dimensions
            #:character #:base-char #:standard-char
            #:complex #:range #:fixnum #:complexp #:complex-part-type
            #:rangep #:range-kind #:range-high #:range-low
