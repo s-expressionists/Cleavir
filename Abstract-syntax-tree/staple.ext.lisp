22f8c2160a18b74954fa849e0e3309a735574af0
Bike <aeshtaer@gmail.com>
2022-11-09 21:51:57 -0500

    Documentation: Remove unreal subsystem relation

--- Abstract-syntax-tree/staple.ext.lisp
+++ Abstract-syntax-tree/staple.ext.lisp
@@ -2,3 +2,8 @@
 
 (defmethod staple:page-type ((sys (eql (asdf:find-system :cleavir-ast))))
   'cleavir-page)
+
+(defmethod staple:subsystems ((sys (eql (asdf:find-system :cleavir-ast))))
+  ;; Without this method, Staple will think cleavir-ast-to-bir is a subsystem, since its
+  ;; name starts with "cleavir-ast".
+  ())
