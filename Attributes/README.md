be7665730cec74e355b4849130f7887ba7b83273
Bike <aeshtaer@gmail.com>
2022-11-15 17:16:04 -0500

    Attributes: document

--- Attributes/README.md
+++ Attributes/README.md
@@ -27,3 +27,20 @@ return from CLEAVIR-ENV:FUNCTION-INFO etc.
 
 TODO: All of this stuff should be more client-customizable.
 Per-argument attributes might be good.
+
+# Flags
+
+A flag is a binary on-off indicating some known information about a function.
+
+Flags are generally organized so that their lack is the general case, i.e. if a flag is "positive" in that it enables transformations, it must be explicitly asserted. Another way of putting this is that a completely unknown function essentially has no flags.
+
+## Currently available flags
+
+`:NO-CALL` means that the function does not increase the number of ways its arguments can be called. That is, it does not call them itself, and does not enable calls to occur in new ways (e.g. by storing an argument in a global variable, where anybody could call it later). This weird phrasing is because function arguments could do these things themselves `(e.g. (labels ((foo (x) (push (cons #'foo x) *calls*))) ...))` and this does not implicate the NO-CALL-ness of any function that is passed them as an argument.
+Implies DYN-CALL.
+
+`:DYN-CALL` means that the function can only increase the number of ways its arguments can be called with ways that call the argument in a dynamic environment substantially identical to that of the `DYN-CALL` function. For example, `(lambda (f) (funcall f))` could be `DYN-CALL`, but `(lambda (f x) (let ((*x* x)) (funcall f)))` could not, as it calls its argument f in a different dynamic environment. This implies that arguments are dx-safe (TODO: attributes for that) because if `f` was e.g. stored in a global it could later be called in arbitrary dynamic environments.
+
+`:DX-CALL` implies that the function's callable arguments do not escape. For example, the function `(lambda (f) (funcall f))` is `DX-CALL`, while `(lambda (f) f)` is not. FIXME: This is probably better expressed as a dynamic extent attribute on individual arguments.
+
+`:FLUSHABLE` means the function does not side-effect, and that calls to it can be deleted if the value of the call is not used.
--- Attributes/attributes.lisp
+++ Attributes/attributes.lisp
@@ -1,22 +1,20 @@
 (in-package #:cleavir-attributes)
 
+(defgeneric identities (attributes-designator)
+  (:documentation "A list of objects used as identifiers for this function. These are used by BIR-TRANSFORMATIONS for client-defined, function-specific transformations, constant folds, type derivations, etc. Their nature is not defined by Cleavir, except that they can be compared with EQUAL, and that they should be externalizable if ASTs are to be externalized.
+One easy choice is to use function names.
+This is a list to account for data flow. For example, if a datum can originate from two separate definitions with different identities, the lists of identities are merged to reach a combined empty list of identities.
+
+See BIR-TRANSFORMATIONS:TRANSFORM-CALL
+See BIR-TRANSFORMATIONS:FOLD-CALL
+See BIR-TRANSFORMATIONS:DERIVE-RETURN-TYPE"))
+
 (defclass attributes ()
   (;; Boolean flags; see flags.lisp
    (%flags :initarg :flags :initform 0 :reader flags :type (integer 0))
-   ;; A list of objects used as identifiers for this function. These are
-   ;; used by BIR-TRANSFORMATIONS for client-defined, function-specific
-   ;; transformations, constant folds, type derivations, etc.
-   ;; Their nature is not defined by Cleavir, except that they can be
-   ;; compared with EQUAL, and that they should be externalizable if ASTs
-   ;; are to be externalized.
-   ;; One easy choice is to use function names.
-   ;; This is a list to account for data flow. For example, if a datum
-   ;; can originate from two separate definitions with different identities,
-   ;; the lists of identities are merged to reach a combined empty list of
-   ;; identities.
-   ;; See BIR-TRANSFORMATIONS:TRANSFORM-CALL, FOLD-CALL, DERIVE-RETURN-TYPE.
    ;; FIXME: Might need some more thought on this.
-   (%identities :initarg :identities :initform nil :reader identities)))
+   (%identities :initarg :identities :initform nil :reader identities))
+  (:documentation "An object representing non-type attributes of a value."))
 
 ;;; We need to be able to externalize attributes for clients that externalize
 ;;; them as part of inline definition ASTs.
@@ -27,22 +25,28 @@
     (:flags (flags attributes))
   (:identities (identities attributes)))
 
-;;; NIL means no special attributes.
-;;; T means all attributes. This will never be the case for any real function,
-;;; but is useful as a theoretical dual to NIL. It's kind of like NIL type.
-(deftype attributes-designator () '(or attributes null (eql t)))
+(deftype attributes-designator ()
+  "A designator for an attributes object, usable where an attributes object would be.
+NIL means no attributes.
+T means all attributes. This will never be the case for any real value, but is useful as a theoretical dual to NIL. It is analogous to the CL:NIL type.
+
+See ATTRIBUTES"
+  '(or attributes null (eql t)))
 
 (defmethod flags ((attr null)) 0)
 (defmethod identities ((attr null)) nil)
 
-(defun default-attributes () nil)
+(defun default-attributes ()
+  "Return the attributes that are safe to assume of any value."
+  nil)
 
 (defmethod flags ((attr (eql t))) -1)
 ;;; Technically this should be all possible identities, but in practice we
 ;;; want to behave as if nothing special is happening.
 (defmethod identities ((attr (eql t))) nil)
 
-(defgeneric has-flag-p (attributes flag-name))
+(defgeneric has-flag-p (attributes-designator flag-name)
+  (:documentation "Return true iff the attributes have the given flag on."))
 
 (defmethod has-flag-p ((attributes null) flag-name)
   (declare (ignore flag-name))
@@ -53,8 +57,8 @@
 (defmethod has-flag-p ((attributes attributes) flag-name)
   (%has-flag-p (flags attributes) flag-name))
 
-;;; Is attributes-1 less specific than attributes-2?
-(defgeneric sub-attributes-p (attributes-1 attributes-2))
+(defgeneric sub-attributes-p (attributes-designator-1 attributes-designator-2)
+  (:documentation "Return true iff attributes-designator-1 is less specific than attributes-designator-2."))
 
 (defmethod sub-attributes-p ((attr1 null) (attr2 null)) t)
 (defmethod sub-attributes-p ((attr1 null) (attr2 attributes)) t)
@@ -68,13 +72,14 @@
 (defmethod sub-attributes-p ((attr1 (eql t)) (attr2 attributes)) nil)
 (defmethod sub-attributes-p ((attr1 (eql t)) (attr2 (eql t))) t)
 
-;;; Return attributes combining both inputs; the returned attributes
-;;; only have a given quality if both of the inputs do. Because attributes
-;;; are of function parameters, they are contravariant, and so this can be
-;;; used like CL:OR types.
-(defgeneric meet-attributes (attributes-1 attributes-2))
-;;; Dual of the above.
-(defgeneric join-attributes (attributes-1 attributes-2))
+(defgeneric meet-attributes (attributes-designator-1 attributes-designator-2)
+  (:documentation "Return attributes combining both inputs; the returned attributes only have a given quality if both of the inputs do.
+
+See JOIN-ATTRIBUTES"))
+(defgeneric join-attributes (attributes-designator-1 attributes-designator-2)
+  (:documentation "Dual to MEET-ATTRIBUTES.
+
+See MEET-ATTRIBUTES"))
 
 (defmethod meet-attributes ((attr1 null) (attr2 null)) attr1)
 (defmethod meet-attributes ((attr1 null) (attr2 attributes)) attr1)
