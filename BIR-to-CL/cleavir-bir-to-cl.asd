16a3c697f2f3d90efcfdf7ce8f9359a8403e8e32
Bike <aeshtaer@gmail.com>
2022-11-23 16:13:15 -0500

    Remove bitrotted IR back-conversion systems
    These have never been very useful, and would take effort to
    properly maintain. BIR-to-CL is also pretty strange due to the
    way control flow is represented at that level; I don't think it
    would ever really be very good.

--- BIR-to-CL/cleavir-bir-to-cl.asd
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:asdf-user)
-
-(defsystem :cleavir-bir-to-cl
-  :depends-on (:cleavir-bir)
-  :components
-  ((:file "packages")
-   (:file "translate" :depends-on ("packages"))))
--- BIR-to-CL/packages.lisp
+++
@@ -1,7 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:cleavir-bir-to-cl
-  (:use #:cl)
-  (:local-nicknames (#:bir #:cleavir-bir)
-                    (#:set #:cleavir-set))
-  (:export #:translate))
